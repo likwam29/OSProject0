407f2f8b0e0148ba9df3bacf149a5250760cd29e
likwam29 <deftblow34@gmail.com>
2017-09-20 18:49:10 -0500

    created header file and populated students array

--- driver.c
+++ driver.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include "student.h"
 
 //Prototyped methods
 
 int getFileSize(FILE* f);
 
 void createFile(char *fileName, int size);
+
+void buildStudentArray(FILE* f, struct student students[], int length);
 
 //Main method
 
@@ -19,7 +22,7 @@ int main ( int argc, char *argv[] )
     else 
     {
         // argv[1] is the file we read from
-        FILE* fp = fopen( argv[1], "r" );
+        FILE* fp = fopen( argv[1], "rb" );
 
         // file failed to open
         if ( fp == 0 )
@@ -28,8 +31,16 @@ int main ( int argc, char *argv[] )
         }
         else 
         {
+			// get the file size and # of records
 		    int fileSize = getFileSize(fp);
+			int numOfStudents = fileSize / 76;
+			printf("%i students\n", numOfStudents);
+			// create array of structs
+			struct student students[numOfStudents];
+			// populate the array of structs
+			buildStudentArray(fp, students, numOfStudents);
 		    printf("%i bytes.\n", fileSize);
+			// create .csv file
 			createFile(argv[2], fileSize);
 		    fclose(fp);
         }
@@ -56,6 +67,25 @@ void createFile(char *fileName, int size)
 	fprintf(fp, "%i", size);
 	
 	fclose(fp);
+}
+
+void buildStudentArray(FILE* f, struct student students[], int length)
+{
+	int i;
+	for(i=0; i<length; i++)
+	{
+		fread(&students[i].Id, sizeof(students[i].Id), 1, f);
+		fread(&students[i].First, sizeof(students[i].First), 1, f);
+		fread(&students[i].Last, sizeof(students[i].Last), 1, f);
+		fread(&students[i].Q1, sizeof(students[i].Q1), 1, f);
+		fread(&students[i].Q2, sizeof(students[i].Q2), 1, f);
+		fread(&students[i].Q3, sizeof(students[i].Q3), 1, f);
+		fread(&students[i].L1, sizeof(students[i].L1), 1, f);
+		fread(&students[i].L2, sizeof(students[i].L2), 1, f);
+		fread(&students[i].A1, sizeof(students[i].A1), 1, f);
+		fread(&students[i].E1, sizeof(students[i].E1), 1, f);
+		fread(&students[i].E2, sizeof(students[i].E2), 1, f);
+	}
 }
 
 
---
+++ student.h
@@ -0,0 +1,15 @@
+
+
+typedef struct student {
+	int Id;
+	char First[20];
+	char Last[20];
+	float Q1;
+	float Q2;
+	float Q3;
+	float L1;
+	float L2;
+	float A1;
+	float E1;
+	float E2;
+}Student;
